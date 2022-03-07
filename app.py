d99ca0113c54c04a4bcc9beea09bc27ed0212757
teresa118 <74627893+teresa118@users.noreply.github.com>
2022-03-07 13:19:33 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+
+import math
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/seleccionar',methods=['GET'])
+def getSeleccionar():
+    operacion = request.args.get('operacion')
+    
+    if(operacion == "formulaGeneralPo"):
+        a = float(request.args.get('a'))
+        b = float(request.args.get('b'))
+        c = float(request.args.get('c'))
+        resultado = (-(b) + ((math.sqrt(b*b)-(4*a*c))))/2*a
+    
+    elif(operacion =="formulaGeneralNe"):
+        a = float(request.args.get('a'))
+        b = float(request.args.get('b'))
+        c = float(request.args.get('c'))
+        resultado = (-(b) - ((math.sqrt(b*b)-(4*a*c))))/2*a
+    
+    elif(operacion =="segundaLey"):
+        masa= float(request.args.get('masa'))
+        aceleracion= float(request.args.get('aceleracion'))
+        resultado=(masa*aceleracion)
+       
+    return jsonify ({"resultado":resultado})
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
