e06b0af2830cf0fa387b8d4c87c6acd71e5d901f
teresa118 <74627893+teresa118@users.noreply.github.com>
2022-03-08 11:41:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,24 +7,19 @@ app = Flask(__name__)
 @app.route('/seleccionar',methods=['GET'])
 def getSeleccionar():
     operacion = request.args.get('operacion')
-    
-    if(operacion == "formulaGeneralPo"):
+
+    if(operacion == "formulaGeneral"):
         a = float(request.args.get('a'))
         b = float(request.args.get('b'))
         c = float(request.args.get('c'))
         resultado = (-(b) + ((math.sqrt(b*b)-(4*a*c))))/2*a
-    
-    elif(operacion =="formulaGeneralNe"):
-        a = float(request.args.get('a'))
-        b = float(request.args.get('b'))
-        c = float(request.args.get('c'))
-        resultado = (-(b) - ((math.sqrt(b*b)-(4*a*c))))/2*a
-    
+        resultado2 = (-(b) - ((math.sqrt(b*b)-(4*a*c))))/2*a
+
     elif(operacion =="segundaLey"):
         masa= float(request.args.get('masa'))
         aceleracion= float(request.args.get('aceleracion'))
-        resultado=(masa*aceleracion)
+        resultado = (masa*aceleracion)
        
-    return jsonify ({"resultado":resultado})
+    return jsonify ({"resultado":resultado,"resultado2":resultado2})
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
